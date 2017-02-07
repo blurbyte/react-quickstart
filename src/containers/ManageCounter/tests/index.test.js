8f4b143fc7e1de29c63d7083fee3950e45439a4f
blurbyte <info@blurbyte.com>
2017-02-07 12:05:45 +0100

    ManageCounter integration test

---
+++ src/containers/ManageCounter/tests/index.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { mount } from 'enzyme';
+
+import ManageCounter from '../index';
+import CounterPanel from '../CounterPanel';
+
+
+import { Provider } from 'react-redux';
+import { browserHistory } from 'react-router';
+import configureStore from '../../../store';
+
+describe('<ManageCounter />', () => {
+  it('should contain <CounterPanel />', () => {
+    const store = configureStore({}, browserHistory);
+    const actions = {
+      increaseCounter: () => { },
+      decreaseCounter: () => { }
+    };
+    const counter = 0;
+
+    const component = mount(
+      <Provider store={store}>
+        <ManageCounter counter={counter} actions={actions} />
+      </Provider>
+    );
+
+    expect(component.find(CounterPanel).length).toEqual(1);
+  });
+});
