926d182cdd107d0d16ccb22dd513bf06713c1c74
blurbyte <info@blurbyte.com>
2017-03-12 10:34:12 +0100

    Updated action creators

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -1,11 +1,3 @@
 //counter action types
 export const INCREASE_COUNTER = 'INCREASE_COUNTER';
 export const DECREASE_COUNTER = 'DECREASE_COUNTER';
-
-//fetch request action types
-export const BEGIN_FETCH_REQUEST = 'BEGIN_FETCH_REQUEST';
-export const FETCH_REQUEST_ERROR = 'FETCH_REQUEST_ERROR';
-export const FETCH_REQUEST_SUCCESS = 'FETCH_REQUEST_SUCCESS';
-
-//example fetch action types
-export const LOAD_DATA_SUCCESS = 'LOAD_DATA_SUCCESS';
