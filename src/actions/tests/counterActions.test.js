a36aa6c19f12149df50ab57345c86bc171287f47
blurbyte <info@blurbyte.com>
2017-03-12 18:48:47 +0100

    Action creators refactored

--- src/actions/tests/counterActions.test.js
+++ src/actions/tests/counterActions.test.js
@@ -3,33 +3,35 @@ import * as types from '../actionTypes';
 
 //synchronous actions BDD tests
 describe('Counter actions', () => {
-  describe('incrementCounter', () => {
-    it('should create INCREMENT_COUNTER action', () => {
+  describe('incrementCounterByAmount', () => {
+    it('should create INCREMENT_COUNTER_BY_AMOUNT action', () => {
       //arrange
       const amount = 2;
       const expectedAction = {
-        type: types.INCREASE_COUNTER,
+        type: types.INCREASE_COUNTER_BY_AMOUNT,
         amount
       };
 
       //act
-      const action = actions.increaseCounter(amount);
+      const action = actions.increaseCounterByAmount(amount);
 
       //assert
       expect(action).toEqual(expectedAction);
     });
   });
   describe('decrementCounter', () => {
-    it('should create DECREASE_COUNTER action', () => {
+    it('should create DECREASE_COUNTER_BY_AMOUNT action', () => {
       //arrange
       const amount = 3;
+      const lowerLimit = 0;
       const expectedAction = {
-        type: types.DECREASE_COUNTER,
-        amount
+        type: types.DECREASE_COUNTER_BY_AMOUNT,
+        amount,
+        lowerLimit
       };
 
       //act
-      const action = actions.decreaseCounter(amount);
+      const action = actions.decreaseCounterByAmount(amount, lowerLimit);
 
       //assert
       expect(action).toEqual(expectedAction);
