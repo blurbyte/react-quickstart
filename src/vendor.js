9a9e946b6f8c2dcff7bf5ed416201e2cefa48924
blurbyte <info@blurbyte.com>
2017-03-12 10:32:59 +0100

    Added redux-saga to vendor modules

--- src/vendor.js
+++ src/vendor.js
@@ -5,7 +5,7 @@ import 'react';
 import 'react-dom';
 import 'redux';
 import 'react-redux';
-import 'redux-thunk';
+import 'redux-saga';
 import 'react-router';
 import 'react-router-redux';
 import 'react-helmet';
