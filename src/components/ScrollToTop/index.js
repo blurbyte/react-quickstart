f616f6662b36f574f672fc326410c112b004b11d
blurbyte <info@blurbyte.com>
2017-03-30 04:27:45 +0200

    ScrollToTop helper component created

---
+++ src/components/ScrollToTop/index.js
@@ -0,0 +1,23 @@
+// helper component which scrolls page to top on each route change
+
+import React, { PropTypes } from 'react';
+import { withRouter } from 'react-router-dom';
+
+class ScrollToTop extends React.Component {
+  componentDidUpdate(prevProps) {
+    if (this.props.location.key !== prevProps.location.key) {
+      window.scrollTo(0, 0);
+    }
+  }
+
+  render() {
+    return this.props.children;
+  }
+}
+
+ScrollToTop.propTypes = {
+  children: PropTypes.node.isRequired,
+  location: PropTypes.object.isRequired
+};
+
+export default withRouter(ScrollToTop);
