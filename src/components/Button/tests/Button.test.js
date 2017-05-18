6ebe989ceb6a0ec0016bef1bbac7649509024361
blurbyte <info@blurbyte.com>
2017-05-18 03:11:46 +0200

    Created Button snapshot tests

---
+++ src/components/Button/tests/Button.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import 'jest-styled-components';
+
+import Button from '../index';
+
+describe('<Button />', () => {
+  it('should render correctly', () => {
+    // component init
+    const component = renderer.create(
+      <Button />
+    );
+
+    expect(component.toJSON()).toMatchStyledComponentsSnapshot();
+  });
+});
