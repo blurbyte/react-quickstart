dcb10b5760cce9a162a58392365978375d9ecb04
blurbyte <info@blurbyte.com>
2017-05-21 12:11:34 +0200

    NotFoundPage snapshot tests created

---
+++ src/containers/NotFoundPage/tests/NotFoundPage.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import NotFoundPage from '../index';
+
+// mocked components;
+jest.mock('react-helmet', () => 'Helmet');
+jest.mock('components/ContentWrapper', () => 'ContentWrapper');
+jest.mock('components/MainHeadline', () => 'MainHeadline');
+jest.mock('components/NavLink', () => 'NavLink');
+
+describe('<NotFoundPage />', () => {
+  it('should render correctly', () => {
+    // component init
+    const component = renderer.create(
+      <NotFoundPage />
+    );
+
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+});
