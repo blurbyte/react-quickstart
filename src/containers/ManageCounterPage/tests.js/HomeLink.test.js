128821e982c93f94cfc540e1b110faa564b65850
blurbyte <info@blurbyte.com>
2017-05-21 12:11:02 +0200

    ManageCounterPage snapshot tests created

---
+++ src/containers/ManageCounterPage/tests.js/HomeLink.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import 'jest-styled-components';
+import { MemoryRouter } from 'react-router-dom';
+
+import HomeLink from '../HomeLink';
+
+describe('<HomeLink />', () => {
+  it('should render correctly', () => {
+    // props init
+    const props = {
+      to: '/home'
+    };
+    // component init
+    const component = renderer.create(
+      <MemoryRouter>
+        <HomeLink {...props} />
+      </MemoryRouter>
+    );
+
+    expect(component.toJSON()).toMatchStyledComponentsSnapshot();
+  });
+});
---
+++ src/containers/ManageCounterPage/tests.js/ManageCounterPage.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import ManageCounterPage from '../index';
+
+// mocked components;
+jest.mock('react-helmet', () => 'Helmet');
+jest.mock('components/ContentWrapper', () => 'ContentWrapper');
+jest.mock('components/MainHeadline', () => 'MainHeadline');
+jest.mock('components/MainParagraph', () => 'MainParagraph');
+jest.mock('../HomeLink', () => 'HomeLink');
+jest.mock('containers/ManageCounter', () => 'ManageCounter');
+
+describe('<ManageCounterPage />', () => {
+  it('should render correctly', () => {
+    // component init
+    const component = renderer.create(
+      <ManageCounterPage />
+    );
+
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+});
