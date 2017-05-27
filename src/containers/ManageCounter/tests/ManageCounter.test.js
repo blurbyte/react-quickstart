50a9fb45a062acbd32cb2149c9c611579a326f34
blurbyte <info@blurbyte.com>
2017-05-28 01:30:53 +0200

    ManageCounter methods tests

--- src/containers/ManageCounter/tests/ManageCounter.test.js
+++ src/containers/ManageCounter/tests/ManageCounter.test.js
@@ -14,8 +14,6 @@ describe('<ManageCounter />', () => {
     // props init
     const props = {
       counter: 10,
-      onIncrease: jest.fn(),
-      onDecrease: jest.fn(),
       increaseCounterByAmount: jest.fn(),
       decreaseCounterByAmount: jest.fn()
     };
@@ -26,18 +24,50 @@ describe('<ManageCounter />', () => {
 
     expect(component.toJSON()).toMatchSnapshot();
   });
-  it('should handle increaseCounterByAmount()', () => {
+  it('should dispatch increaseCounterByAmount()', () => {
     //init
     const amount = 10;
     actions.increaseCounterByAmount(amount);
 
     expect(dispatch.mock.calls).toMatchSnapshot();
   });
-  it('should handle increaseCounterByAmount()', () => {
+  it('should dispatch decreaseCounterByAmount()', () => {
     const amount = 0;
     const lowerLimit = 0;
     actions.decreaseCounterByAmount(amount, lowerLimit);
 
     expect(dispatch.mock.calls).toMatchSnapshot();
+  });
+  it('should dispatch increaseCounterByAmount() once when handling onIncrease()', () => {
+    // props init
+    const props = {
+      counter: 0,
+      increaseCounterByAmount: jest.fn(),
+      decreaseCounterByAmount: jest.fn()
+    };
+    // component init
+    const component = renderer.create(
+      <ManageCounter {...props} />
+    );
+    const instance = component.getInstance();
+    instance.onIncrease();
+
+    expect(props.increaseCounterByAmount.mock.calls.length).toEqual(1);
+  });
+  it('should dispatch decreaseCounterByAmount() once when handling onDecrease()', () => {
+    // props init
+    const props = {
+      counter: 20,
+      increaseCounterByAmount: jest.fn(),
+      decreaseCounterByAmount: jest.fn()
+    };
+    // component init
+    const component = renderer.create(
+      <ManageCounter {...props} />
+    );
+    const instance = component.getInstance();
+    instance.onDecrease();
+
+    expect(props.decreaseCounterByAmount.mock.calls.length).toEqual(1);
   });
 });
