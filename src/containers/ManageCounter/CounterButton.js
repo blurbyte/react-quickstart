36f3262678371ab6b3df7d7a5948b2cf942d9f19
blurbyte <info@blurbyte.com>
2017-05-27 20:49:31 +0200

    Updated CounterButton component

--- src/containers/ManageCounter/CounterButton.js
+++ src/containers/ManageCounter/CounterButton.js
@@ -1,28 +1,11 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styled from 'styled-components';
-
-
 import Button from 'components/Button';
 
-const StyledButton = styled(Button)`
+const CounterButton = Button.extend`
   margin-right: 1.4rem;
 
   &:last-child {
     margin-right: 0;
   }
 `;
-
-const CounterButton = ({ disabled, children, onClick }) => (
-  <StyledButton onClick={onClick} disabled={disabled}>
-    {children}
-  </StyledButton>
-);
-
-CounterButton.propTypes = {
-  disabled: PropTypes.bool,
-  children: PropTypes.node,
-  onClick: PropTypes.func
-};
 
 export default CounterButton;
